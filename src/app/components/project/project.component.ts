f8b437a7d3382c56b3b8c14fe2fa31af82b0a9ca
alexnguetcha <nguetchaalex@gmail.com>
2022-03-16 07:45:32 +0100

    add projects

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -12,36 +12,40 @@ export class ProjectComponent implements OnInit {
     this.projects = [
       {
         name: "Angular Portfolio",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
+        description: "My personal portfolio build with Angular.",
         picture: "./assets/project/portfolio.png",
         technos : ["angular", "typescript", "html", "css"],
         github: "https://github.com/AlexNguetcha/portfolio",
-        link: "https://dashboard.heroku.com/apps"
+        link: "https://alexnguetcha.github.io/portfolio/"
       },
       {
-        name: "Angular Portfolio",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
-        picture: "./assets/project/portfolio.png",
-        technos : ["angular", "typescript", "html", "css"],
-        github: "https://github.com/AlexNguetcha/portfolio",
-        link: "https://dashboard.heroku.com/apps"
+        name: "Food delivery app",
+        description: "Development of a food ordering application for an internal university restaurant.",
+        picture: "./assets/project/nyaama.png",
+        technos : ["php", "java", "javascript", "xml", "html", "css"],
+        
       },
       {
-        name: "Angular Portfolio",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
-        picture: "./assets/project/portfolio.png",
-        technos : ["angular", "typescript", "html", "css"],
-        github: "https://github.com/AlexNguetcha/portfolio",
-        link: "https://dashboard.heroku.com/apps"
+        name: "Weekly and Daily Planner",
+        description: "Weekly and Daily Planner is an application that allows you to plan your shedule each week in a simple and efficient way.",
+        picture: "./assets/project/planner.jpg",
+        technos : ["java", "xml"],
+        link: "https://play.google.com/store/apps/details?id=com.larvablog.umanager"
       },
       {
-        name: "Angular Portfolio",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae ipsum nam debitis, laudantium sapiente ipsa a quam, ratione asperiores maxime aspernatur possimus autem itaque eligendi eius explicabo beatae qui!",
-        picture: "./assets/project/portfolio.png",
-        technos : ["angular", "typescript", "html", "css"],
-        github: "https://github.com/AlexNguetcha/portfolio",
-        link: "https://dashboard.heroku.com/apps"
-      }
+        name: "OKAMPUS",
+        description: "Development of a school help application for students from different faculties allowing them to have: the latest information, old tests and job offers",
+        picture: "./assets/project/okampus.png",
+        technos : ["php", "javascript", "html", "css"],
+      },
+      {
+        name: "Quizzle - Jeu de mots",
+        description: "Quizzle is a word game of finding the correct word from a shuffled word",
+        picture: "./assets/project/quizzle.png",
+        technos : ["java", "xml"],
+        link: "https://play.google.com/store/apps/details?id=com.larvablog.quizzle"
+      },
+      
       
     ];
   }
